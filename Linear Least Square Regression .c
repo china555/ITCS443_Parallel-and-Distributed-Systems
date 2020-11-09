6d22e61c6da5038ae6713a4cb2f024e362916e5b
Chinapanda <kittikornchina2014@gmail.com>
2020-11-09 12:17:03 +0700

    change a logic to assign value in each variable

--- Linear Least Square Regression .c
+++ Linear Least Square Regression .c
@@ -7,8 +7,6 @@ int main(int argc, char *argv[])
 {
     int xPowerOfTwo[N];
     int xMultipleByY[N];
-    int leftEdge = 1;
-    int rightEdge = N;
     int x[N] = {1, 3, 5, 7, 10, 12, 14, 16};
     int y[N] = {2, 4, 6, 8, 9, 11, 13, 15};
     int rankForEachThread, sizeOfThread, tag = 123, root = 0, i, interval, start, end;
@@ -28,26 +26,18 @@ int main(int argc, char *argv[])
     // }
     // MPI_Bcast(x, N, MPI_INT, root, MPI_COMM_WORLD);
     // MPI_Bcast(y, N, MPI_INT, root, MPI_COMM_WORLD);
-    if (rankForEachThread > 0)
+
+    for (number = rankForEachThread; number < N; number = number + sizeOfThread)
     {
-        interval = (rightEdge - leftEdge + 1) / (sizeOfThread - 1);
-        start = (rankForEachThread - 1) * interval;
-        end = start + interval - 1;
-        if (rankForEachThread == (sizeOfThread - 1))
-        {
-            end = rightEdge;
-        }
-        for (number = start; number <= end; number++)
-        {
-            xPowerOfTwo[start] = pow(x[start], 2);
-            xMultipleByY[start] = x[start] * y[start];
-            sumX = sumX + x[start];
-            sumY = sumY + y[start];
-            sumXPowerOfTwo = sumXPowerOfTwo + xPowerOfTwo[start];
-            sumXMultipleByY = sumXMultipleByY + xMultipleByY[start];
-            printf("The process %d do x^2 = %d\n x*y = %d\nsumx = %d\nsumy = %d\nsumXPowerOfTwo = %d\nsumXMultipleByY = %d", rankForEachThread, xPowerOfTwo[start], xMultipleByY, sumX, sumY, sumXPowerOfTwo, sumXMultipleByY);
-        }
+        xPowerOfTwo[number] = pow(x[number], 2);
+        xMultipleByY[number] = x[number] * y[number];
+        sumX = sumX + x[number];
+        sumY = sumY + y[number];
+        sumXPowerOfTwo = sumXPowerOfTwo + xPowerOfTwo[number];
+        sumXMultipleByY = sumXMultipleByY + xMultipleByY[number];
+        printf("The process %d do x^2 = %d\n x*y = %d\nsumx = %d\nsumy = %d\nsumXPowerOfTwo = %d\nsumXMultipleByY = %d", rankForEachThread, xPowerOfTwo[start], xMultipleByY, sumX, sumY, sumXPowerOfTwo, sumXMultipleByY);
     }
+
     MPI_Finalize();
     return 0;
 }
