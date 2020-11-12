8fc5c82ab0f8896023dced8a1f9e8cf44ef2a2c2
Nattawipa Saetae <nattawipa.sae@student.mahidol.ac.th>
2020-11-12 23:15:42 +0700

    update

--- Project2/Linear_Least_Square_Regression.c
+++ Project2/Linear_Least_Square_Regression.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "mpi.h"
 
 #define N 4
 
 int main(int argc, char *argv[])
 {
-	int xPowerOfTwo[N], xMultipleByY[N], x[N], y[N], rankForEachThread, sizeOfThread, tag = 123, root = 0;
+	int xPowerOfTwo[N], xMultipleByY[N], x[N], y[N], rankForEachThread, sizeOfThread, tag = 123, root = 0,i;
 	MPI_Status status;
 	int sumX = 0, sumY = 0, sumXPowerOfTwo = 0, sumXMultipleByY = 0;
 	int resultsumX = 0, resultsumY = 0, resultsumXPowerOfTwo = 0, resultsumXMultipleByY;
@@ -13,7 +15,9 @@ int main(int argc, char *argv[])
 	MPI_Comm_size(MPI_COMM_WORLD, &sizeOfThread);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rankForEachThread);
 	float m = 0.0, m1 = 0.0, m2 = 0.0, b = 0.0, b1 = 0.0, b2 = 0.0;
-	float resultY[N]; //final y=mx+b
+	float resultY[N];
+	FILE *outFile;
+
 	if (rankForEachThread == 0)
 	{
 		srand(time(NULL));
@@ -23,8 +27,10 @@ int main(int argc, char *argv[])
 			y[i] = rand() % 100 + 1;
 		}
 	}
+
 	MPI_Bcast(x, N, MPI_INT, root, MPI_COMM_WORLD);
 	MPI_Bcast(y, N, MPI_INT, root, MPI_COMM_WORLD);
+
 	for (int number = rankForEachThread; number < N; number = number + sizeOfThread)
 	{
 		xPowerOfTwo[number] = x[number] * x[number];
@@ -35,7 +41,7 @@ int main(int argc, char *argv[])
 		sumXMultipleByY = sumXMultipleByY + xMultipleByY[number];
 		printf("The process %d\ndo x^2 = %d\n x*y = %d\nsumx = %d\nsumy = %d\nsumXPowerOfTwo = %d\nsumXMultipleByY = %d\n", rankForEachThread, xPowerOfTwo[number], xMultipleByY[number], sumX, sumY, sumXPowerOfTwo, sumXMultipleByY);
 	}
-	//----reduce----
+	
 	MPI_Reduce(&sumX, &resultsumX, 1, MPI_INT, MPI_SUM, root, MPI_COMM_WORLD);
 	MPI_Reduce(&sumY, &resultsumY, 1, MPI_INT, MPI_SUM, root, MPI_COMM_WORLD);
 	MPI_Reduce(&sumXPowerOfTwo, &resultsumXPowerOfTwo, 1, MPI_INT, MPI_SUM, root, MPI_COMM_WORLD);
@@ -43,21 +49,21 @@ int main(int argc, char *argv[])
 
 	if (rankForEachThread == 0)
 	{
-		printf("\n\nThe process %d\nsumx = %d\nsumy = %d\nsumXPowerOfTwo = %d\nsumXMultipleByY = %d\n", rankForEachThread, resultsumX, resultsumY, resultsumXPowerOfTwo, resultsumXMultipleByY);
+		printf("\n\n------------------ PROCESS %d ------------------ \nSum of all x = %d\nSum of all y = %d\nSum of all X PowerOfTwo = %d\nSum of all X MultipleBy Y = %d\n", rankForEachThread, resultsumX, resultsumY, resultsumXPowerOfTwo, resultsumXMultipleByY);
 
 		m1 = (N * resultsumXMultipleByY) - (resultsumX * resultsumY);
 		m2 = (N * resultsumXPowerOfTwo) - (resultsumX * resultsumX);
 		m = m1 / m2;
 
-		printf("---------- Calculate M ----------\nM1 = %.2f\nM2 = %.2f\nM1/M2 = %.2f\n", m1, m2, m);
-
+		printf("-------------- Calculate Slope m --------------\nm1 = %.2f\nm2 = %.2f\nm = m1/m2 = %.2f\n", m1, m2, m);
+			
 		b1 = resultsumY - (m * resultsumX);
 		b2 = N;
 		b = b1 / b2;
 
-		printf("---------- Calculate B ----------\nb1 = %.2f\nb2 = %.2f\nb1/b2 = %.2f\n", b1, b2, b);
+		printf("------------ Calculate Intercept b ------------\nb1 = %.2f\nb2 = %.2f\nb = b1/b2 = %.2f\n", b1, b2, b);
 
-		printf("---------- Calculate Y ----------\n");
+		printf("---------- Calculate Equation line Y ----------\n");
 	}
 
 	MPI_Bcast(&m, 1, MPI_FLOAT, root, MPI_COMM_WORLD);
@@ -70,10 +76,22 @@ int main(int argc, char *argv[])
 
 	if (rankForEachThread == 0)
 	{
+		//for (int i = 0; i < N; i++)
+		//{
+		//	printf("if x = %d\t Y [%d] = %d\n", x[i], i, y[i]);
+		//}
 		for (int i = 0; i < N; i++)
 		{
 			printf("if x = %d\t Y [%d] = %.2f\n", x[i], i, resultY[i]);
 		}
+		
+		outFile = fopen("PlotGraph.csv","w");
+		for(int i = 0; i < N; i++)	
+		{
+			fprintf(outFile,"%d , %f\n",x[i],resultY[i]);
+		}
+		fclose(outFile);
 	}
+	
 	MPI_Finalize();
 }
